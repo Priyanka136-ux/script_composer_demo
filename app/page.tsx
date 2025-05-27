183912bf47589a1516099d9fe53a5770fdd4e7be
Priyanka136-ux <priyankacse136@gmail.com>
2025-05-27 11:28:05 +0530

    feat: init commit to explain script composer issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,71 @@
-import Image from "next/image";
+"use client";
+import {
+  Account,
+  Aptos,
+  AptosConfig,
+  Ed25519PrivateKey,
+  Network,
+} from "@aptos-labs/ts-sdk";
+import {
+  CallArgument,
+  scriptComposerSimpleTransaction,
+} from "aptos-script-composer-ts-sdk";
+import DELAGATE_SCRIPT_ABI from "@/app/abi/delegatedScripts.json";
 
+const walletAddress =
+  "0x59c3ce271da339c1629dc41e826a30ed4bc77654d56e5fda6143ad8fc103ea29";
+export const marketIds = [1338, 1339, 1340];
 export default function Home() {
+  const account = Account.fromPrivateKey({
+    privateKey: new Ed25519PrivateKey(
+      process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY as string
+    ),
+  });
+  console.log("account", account.accountAddress.toString());
+  const config = new AptosConfig({
+    network: Network.TESTNET,
+  });
+  const aptos = new Aptos(config);
+
+  const handleScriptComposeSimulation = async () => {
+    const transaction = await scriptComposerSimpleTransaction({
+      sender: account?.accountAddress,
+      builder: async (builder: any) => {
+        for (const market of marketIds) {
+          await builder.addBatchedCalls({
+            function:
+              "0x7a38039fffd016adcac2c53795ee49325e5ec6fddf3bf02651c09f9a583655a6::delegated_scripts::update_delegated_position",
+            functionArguments: [
+              CallArgument.newSigner(0),
+              walletAddress,
+              market,
+            ],
+            typeArguments: [],
+            moduleAbi: DELAGATE_SCRIPT_ABI,
+            config: config,
+          });
+        }
+        return builder;
+      },
+      options: {
+        gasUnitPrice: 100,
+        maxGasAmount: 5000,
+      },
+      withFeePayer: true,
+      config: config,
+    });
+    // simulate
+    const sim = await aptos.transaction.simulate.simple({
+      transaction,
+    });
+    console.log("simulate", sim);
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <div onClick={handleScriptComposeSimulation}>Simulate</div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }

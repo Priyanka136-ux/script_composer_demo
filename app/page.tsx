3104533c70d3271478a869aa226e1fb67b5bf877
WGB5445 <919603023@qq.com>
2025-05-28 13:17:21 +0800

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,45 @@
 "use client";
 import {
-  Account,
   Aptos,
   AptosConfig,
-  Ed25519PrivateKey,
-  Network,
-} from "@aptos-labs/ts-sdk";
-import {
   CallArgument,
-  scriptComposerSimpleTransaction,
-} from "aptos-script-composer-ts-sdk";
-import DELAGATE_SCRIPT_ABI from "@/app/abi/delegatedScripts.json";
+  Network,
+  
+} from "@aptos-labs/ts-sdk";
 
 const walletAddress =
   "0x59c3ce271da339c1629dc41e826a30ed4bc77654d56e5fda6143ad8fc103ea29";
-export const marketIds = [1338, 1339, 1340];
 export default function Home() {
-  const account = Account.fromPrivateKey({
-    privateKey: new Ed25519PrivateKey(
-      process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY as string
-    ),
-  });
-  console.log("account", account.accountAddress.toString());
+  // const account = Account.fromPrivateKey({
+  //   privateKey: new Ed25519PrivateKey(
+  //     process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY as string
+  //   ),
+  // });
+  // console.log("account", account.accountAddress.toString());
   const config = new AptosConfig({
     network: Network.TESTNET,
   });
   const aptos = new Aptos(config);
 
   const handleScriptComposeSimulation = async () => {
-    const transaction = await scriptComposerSimpleTransaction({
-      sender: account?.accountAddress,
-      builder: async (builder: any) => {
-        for (const market of marketIds) {
+
+    let transaction = await aptos.transaction.build.scriptComposer({
+      sender:"0x1",
+      builder: async (builder) => {
           await builder.addBatchedCalls({
             function:
-              "0x7a38039fffd016adcac2c53795ee49325e5ec6fddf3bf02651c09f9a583655a6::delegated_scripts::update_delegated_position",
+              "0x1::aptos_account::transfer",
             functionArguments: [
               CallArgument.newSigner(0),
               walletAddress,
-              market,
+              1
             ],
-            typeArguments: [],
-            moduleAbi: DELAGATE_SCRIPT_ABI,
-            config: config,
+            typeArguments: []
           });
-        }
+        
         return builder;
-      },
-      options: {
-        gasUnitPrice: 100,
-        maxGasAmount: 5000,
-      },
-      withFeePayer: true,
-      config: config,
-    });
+      }, })
+     
     // simulate
     const sim = await aptos.transaction.simulate.simple({
       transaction,
